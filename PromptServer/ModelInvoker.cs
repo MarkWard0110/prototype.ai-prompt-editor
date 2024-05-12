d1e0288bdc4376b99079100bf2ade1ed824cb997
Mark Ward <mark.ward@binaryward.com>
2024-05-12 13:16:59 -0500

    improve response handling and add a timeout to handle when a model does not respond in an expected amount of time.

--- PromptServer/ModelInvoker.cs
+++ PromptServer/ModelInvoker.cs
@@ -5,16 +5,16 @@ using System.Threading.Tasks;
 using OllamaSharp;
 using OllamaSharp.Models;
 
-public class ModelInvoker: IModelInvoker
+public class ModelInvoker : IModelInvoker
 {
     private OllamaApiClient _ollama;
     private Task? _processingTask;
     private CancellationTokenSource? _cancellationTokenSource;
 
     private readonly ConcurrentQueue<ModelRequest> _requestQueue = new ConcurrentQueue<ModelRequest>();
-    private readonly SemaphoreSlim _modelSemaphore; 
+    private readonly SemaphoreSlim _modelSemaphore;
 
-    public ModelInvoker(string ollamaApiUrl,int maxConcurrentInvocations)
+    public ModelInvoker(string ollamaApiUrl, int maxConcurrentInvocations)
     {
         _ollama = new OllamaApiClient(ollamaApiUrl);
         _modelSemaphore = new SemaphoreSlim(maxConcurrentInvocations);
@@ -47,7 +47,7 @@ public class ModelInvoker: IModelInvoker
         _requestQueue.Enqueue(request);
         return tcs.Task;
     }
-    
+
     private async Task ProcessQueueAsync(CancellationToken cancellationToken)
     {
         while (!cancellationToken.IsCancellationRequested)
@@ -65,10 +65,22 @@ public class ModelInvoker: IModelInvoker
                         {
 
                             Temperature = request.Temperature,
-                            Stop = request.Stop 
+                            Stop = request.Stop
                         }
                     };
-                    var modelResponse = await _ollama.GetCompletion(modelRequest);
+
+                    var timeout = TimeSpan.FromMinutes(10);
+                    using var cts = new CancellationTokenSource(timeout);
+                   
+                    ConversationContextWithResponse? modelResponse = null;
+                    try
+                    {
+                        modelResponse = await _ollama.GetCompletion(modelRequest, cts.Token);   
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        modelResponse = new ConversationContextWithResponse("<timeout>", [], null);
+                    }
 
                     request.CompletionSource.SetResult(modelResponse.Response);
                 }
@@ -92,8 +104,8 @@ public class ModelInvoker: IModelInvoker
 class ModelRequest
 {
     public TaskCompletionSource<string> CompletionSource { get; }
-    
-    public string Model {get;}
+
+    public string Model { get; }
     public string Prompt { get; }
 
     public float Temperature { get; }
